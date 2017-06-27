2e6340d60614518bca7cf547b6886b20b39c35d6
Maxim Khatskevich <maxim@khatskevi.ch>
2017-06-28 04:38:09 +0500

    Added header documentation

--- Sources/AssociatedStorage.swift
+++ Sources/AssociatedStorage.swift
@@ -2,6 +2,9 @@ import Foundation
 
 //===
 
+/**
+ Conformance to this protocl guarantees that the type can be initialized on demand with simple `init()` constructor with no aprameters.
+ */
 public
 protocol SimplyInitializable: class
 {
@@ -10,6 +13,9 @@ protocol SimplyInitializable: class
 
 //===
 
+/**
+ Conformance to this protocl guarantees that the type can be initialized on demand with constructor that takes the key object as the only parameter.
+ */
 public
 protocol KeyObjectInitializable: class
 {
@@ -20,17 +26,27 @@ protocol KeyObjectInitializable: class
 
 //===
 
+/**
+ Key-value storage where value object will be automatically released when key object is deallocated.
+ 
+ This storage for any given object of `Key` type (`keyObject`) allows to create and store one and only one instance of `Value` object type (`valueObject`). Each `valueObject`is lazy-initialized on first access and then being stored in the storage till the end of  corresponding `keyObject` life cycle. This special technique guarantees that the `valueObject` will be available while its `keyObject` is in memory, but, at the same time, will be released from memory as soon as its `keyObject` released, preventing memory leaks, but providing great convenience of dynamically extending any type storage capabiolities on demand.
+ */
 public
 struct AssociatedStorage
 {
+    public
+    init() { }
+    
+    /**
+     Internal storage that actually stores the objects.
+     
+     - Note: The storage is initialized with `keyOptions` set to `weakMemory` and `valueOptions` set to `strongMemory`, which means each value is being kept in this collection as long as the object that is used as key is being kept in memory; as soon as the `key` object is deallocated the whole record automatically being removed from the storage.
+     */
     fileprivate
     var storage = NSMapTable<AnyObject, AnyObject>(
         keyOptions: .weakMemory,
         valueOptions: .strongMemory
     )
-    
-    public
-    init() { }
 }
 
 //=== MARK: SimplyInitializable value
@@ -38,6 +54,43 @@ struct AssociatedStorage
 public
 extension AssociatedStorage
 {
+    /**
+     Provides access to the value object, associated with given key object.
+     
+     Use as follows:
+     
+     ```swift
+     class Owner
+     {
+         // ...
+     }
+     
+     class Dependent: SimplyInitializable
+     {
+        init()
+        {
+            // ...
+        }
+     }
+     
+     let owner: Owner = //... object that is being held in memory elsewhere
+     
+     let dependentForTheOwner: Dependent = AssociatedStorage.get(for: owner)
+     
+     //...
+     
+     // later:
+     
+     let dependentAgain: Dependent = AssociatedStorage.get(for: owner)
+     
+     // `dependentForTheOwner` is absolutely the same object as `dependentAgain`
+     
+     ```
+     
+     - Parameter keyObject: Object that is being held in memory somewhere and for which it's required to return associated object of type `Value`.
+     
+     - Returns: Object associated with given `keyObject`. This object is lazy-instantiated using `init()` constructor required by `SimplyInitializable` protocol.
+     */
     func get<Key, Value>(for keyObject: Key) -> Value where
         Key: AnyObject,
         Value: SimplyInitializable
@@ -63,6 +116,43 @@ extension AssociatedStorage
 public
 extension AssociatedStorage
 {
+    /**
+     Provides access to value object, associated with given key object.
+     
+     Use as follows:
+     
+     ```swift
+     class Owner
+     {
+        // ...
+     }
+     
+     class Dependent: KeyObjectInitializable
+     {
+        init(with keyObject: Owner)
+        {
+            //...
+        }
+     }
+     
+     let owner: Owner = // ... object that is being held in memory elsewhere
+     
+     let dependentForTheOwner: Dependent = AssociatedStorage.get(for: owner)
+     
+     //...
+     
+     // later:
+     
+     let dependentAgain: Dependent = AssociatedStorage.get(for: owner)
+     
+     // `dependentForTheOwner` is absolutely the same object as `dependentAgain`
+     
+     ```
+     
+     - Parameter keyObject: Object that is being held in memory somewhere and for which it's required to return associated object of type `Value`.
+     
+     - Returns: Object associated with given `keyObject`. This object is lazy-instantiated using `init(with: Key)` constructor required by `KeyObjectInitializable` protocol.
+     */
     func get<Key, Value>(for keyObject: Key) -> Value where
         Key: AnyObject,
         Value: KeyObjectInitializable,
@@ -89,6 +179,37 @@ extension AssociatedStorage
 public
 extension AssociatedStorage
 {
+    /**
+     Explicitly puts into storage given value object associated with given key object.
+     
+     Use as follows:
+     
+     ```swift
+     class Owner
+     {
+         // ...
+     }
+     
+     class Dependent
+     {
+         // ...
+     }
+     
+     let owner: Owner = // ... object that is being held in memory elsewhere
+     
+     let dependentForTheOwner: Dependent = // create somehow...
+     
+     AssociatedStorage.set(dependentForTheOwner, for: owner)
+     
+     let dependentAgain: Dependent = AssociatedStorage.get(for: owner)
+     
+     // `dependentForTheOwner` is absolutely the same object as `dependentAgain`
+     
+     ```
+     - Parameter valueObject: Object that must be stored as associtated value for `keyObject`.
+     
+     - Parameter keyObject: Object that is being held in memory somewhere and for which it's required to return associated object of type `Value`.
+     */
     func set<Key, Value>(_ valueObject: Value, for keyObject: Key) where
         Key: AnyObject,
         Value: AnyObject
@@ -96,6 +217,37 @@ extension AssociatedStorage
         storage.setObject(valueObject, forKey: keyObject)
     }
     
+    /**
+     Provides access to value object, associated with given key object.
+     
+     Use as follows:
+     
+     ```swift
+     class Owner
+     {
+        // ...
+     }
+     
+     class Dependent
+     {
+        // ...
+     }
+     
+     let owner: Owner = // ... object that is being held in memory elsewhere
+     
+     let dependentForTheOwner: Dependent = AssociatedStorage.get(for: owner)
+     
+     let dependentAgain: Dependent = AssociatedStorage.get(for: owner)
+     
+     // `dependentForTheOwner` is the same object as `dependentAgain`,
+     // or `nil` if associated object is not set yet
+     
+     ```
+     
+     - Parameter keyObject: Object that is being held in memory somewhere and for which it's required to return associated object of type `Value`.
+     
+     - Returns: Object associated with given `keyObject`, or `nil` if associated object is not set yet by `set(...)` function.
+     */
     func get<Key, Value>(for keyObject: Key) -> Value? where
         Key: AnyObject,
         Value: AnyObject
@@ -118,6 +270,40 @@ extension AssociatedStorage
 public
 extension AssociatedStorage
 {
+    /**
+     Provides access to value object, associated with given key object.
+     
+     Use as follows:
+     
+     ```swift
+     class Owner
+     {
+         // ...
+     }
+     
+     class Dependent
+     {
+         // ...
+     }
+     
+     let owner: Owner = //... object that is being held in memory elsewhere
+     
+     let dependentForTheOwner: Dependent = AssociatedStorage.get(for: owner){ key in
+     
+        // ... create and return somehow an instance of Dependent here,
+        // with or without usage of the `key` object;
+        // this closure must be passed every time when access valu eobjects of
+        // type `Dependent` via this particular variation of `get(...)` function,
+        // because otherwise it's unclear how else to instantiate `valueObject`
+        // on first access.
+     }
+     
+     ```
+     
+     - Parameter keyObject: Object that is being held in memory somewhere and for which it's required to return associated object of type `Value`.
+     
+     - Returns: Object associated with given `keyObject`. This object is lazy-instantiated using `initialization` closure on first access.
+     */
     func get<Key, Value>(
         for keyObject: Key,
         initialization: (Key) -> Value
